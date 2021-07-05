8f8ee9620c7f80164cb89155eca596561d5fbeea
MattiaFanan <mattia.fanan@gmail.com>
2021-07-05 18:39:43 +0200

    tried rgb_planes->or and clahe

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,12 @@ using namespace saliency;
 
 int main() {
     //Mat img = imread("/home/mattia/CLionProjects/CV/BoatDetector/FINAL_DATASET/TRAINING_DATASET/IMAGES/image0147.png");
-    Mat img = imread("/home/mattia/CLionProjects/CV/BoatDetector/FINAL_DATASET/TEST_DATASET/venice/06.png");
+    Mat img = imread("/home/mattia/CLionProjects/CV/BoatDetector/FINAL_DATASET/TEST_DATASET/venice/05.png");
     //Mat img = imread("/home/mattia/CLionProjects/CV/BoatDetector/FINAL_DATASET/TEST_DATASET/kaggle/01.jpg");
-    cvtColor(img, img, COLOR_RGB2GRAY);
     Ptr<saliency::StaticSaliencySpectralResidual> pointer = saliency::StaticSaliencySpectralResidual::create();
+
+
+    /*cvtColor(img, img, COLOR_RGB2GRAY);
     Mat out;
     Mat threshold_out;
     pointer->computeSaliency(img, out);
@@ -24,7 +26,48 @@ int main() {
     imshow("TMP", img);
     waitKey(0);
     imshow("TMP", threshold_out);
+    waitKey(0);*/
+
+    Mat brg[3];
+    Mat brg_sal[3];
+    split(img,brg);
+
+    for( int i = 0 ; i<3 ; i++) {
+        pointer->computeSaliency(brg[i], brg_sal[i]);
+        brg_sal[i].convertTo(brg_sal[i], CV_8UC1, 255);
+        threshold(brg_sal[i], brg_sal[i], 0, 255, THRESH_BINARY | THRESH_OTSU);
+    }
+
+    Mat or_img;
+    bitwise_or(brg_sal[0],brg_sal[1],or_img);
+    bitwise_or(or_img,brg_sal[2],or_img);
+
+    imshow("orig",img);
+    //imshow("b",brg_sal[0]);
+    //imshow("r",brg_sal[1]);
+    //imshow("g",brg_sal[2]);
+    imshow("or",or_img);
+
+    Mat g;
+    pointer->computeSaliency(img, g);
+    g.convertTo(g, CV_8UC1, 255);
+    threshold(g, g, 0, 255, THRESH_BINARY | THRESH_OTSU);
+    imshow("img",g);
+
+    Ptr<CLAHE> clahe = createCLAHE();
+    clahe->setClipLimit(4);
+    Mat cla_img;
+    cvtColor(img,cla_img,COLOR_RGB2GRAY);
+    clahe->apply(cla_img,cla_img);
+    pointer->computeSaliency(cla_img, cla_img);
+    g.convertTo(cla_img, CV_8UC1, 255);
+    threshold(cla_img, cla_img, 0, 255, THRESH_BINARY | THRESH_OTSU);
+    imshow("CLAHE",cla_img);
+
+
     waitKey(0);
+
+
 
     /*
     int erosion_size = 1;
