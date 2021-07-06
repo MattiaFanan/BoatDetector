f3459c337588c465c2aa3620d80ee0715cc8b274
MattiaFanan <mattia.fanan@gmail.com>
2021-07-06 20:35:56 +0200

    too complex scene for edge based segmentation

--- src/main.cpp
+++ src/main.cpp
@@ -8,64 +8,42 @@ using namespace cv;
 using namespace std;
 
 int main() {
-    //Mat img = imread("/home/mattia/CLionProjects/CV/BoatDetector/FINAL_DATASET/TRAINING_DATASET/IMAGES/image0147.png");
-    Mat img = imread("/home/mattia/CLionProjects/CV/BoatDetector/FINAL_DATASET/TEST_DATASET/venice/05.png");
-    //Mat img = imread("/home/mattia/CLionProjects/CV/BoatDetector/FINAL_DATASET/TEST_DATASET/kaggle/01.jpg");
-    imshow("origin", img);
-    Mat hsv_img;
-    cvtColor(img,hsv_img,COLOR_BGR2HSV);
-    Mat rows_ch = Mat::zeros(img.size[0], img.size[1], CV_32FC1);
-    Mat cols_ch = Mat::zeros(img.size[0], img.size[1], CV_32FC1);
+    //Mat src = imread("/home/mattia/CLionProjects/CV/BoatDetector/FINAL_DATASET/TRAINING_DATASET/IMAGES/image0147.png");
+    Mat src = imread("/home/mattia/CLionProjects/CV/BoatDetector/FINAL_DATASET/TEST_DATASET/venice/05.png");
+    //Mat src = imread("/home/mattia/CLionProjects/CV/BoatDetector/FINAL_DATASET/TEST_DATASET/kaggle/01.jpg");
+    imshow("origin", src);
 
-    for(int c=0; c < rows_ch.cols; c++)
-        for(int r=0; r < rows_ch.rows; r++) {
-            rows_ch.at<float>(r, c) = r ;
-            cols_ch.at<float>(r, c) = c ;
-        }
+    Mat src_gray;
+    Mat dst, detected_edges;
+    int lowThreshold = 70;
+    const int max_lowThreshold = 200;
+    const int kernel_size = 3;
+    const char* window_name = "Edge Map";
 
-    Mat data;
-    hsv_img.convertTo(data,CV_32F);
+    cvtColor(src,src_gray,COLOR_BGR2GRAY);
+    GaussianBlur( src_gray, detected_edges, Size(3,3),2);
+    Canny( detected_edges, detected_edges, lowThreshold, max_lowThreshold, kernel_size );
+    dst = Scalar::all(0);
+    src_gray.copyTo( dst, detected_edges);
+    //dilate(dst,dst, getStructuringElement(MORPH_RECT,Size(2,2)));
 
-    vector<Mat> channels,tmp;
-    split(data,tmp);
-    for(auto &ch : tmp)
-        channels.push_back(ch);
-    channels.push_back(rows_ch);
-    channels.push_back(cols_ch);
-    merge(channels,data);
-
-    cout << hsv_img.channels()<<endl;
-    cout << data.channels()<<endl;
-
-    data = data.reshape(1,data.rows * data.cols);
-
-    int numLabels = 6;
-    // do kmeans
-    Mat labels, centers;
-    kmeans(data, numLabels, labels, TermCriteria(TermCriteria::MAX_ITER, 10, 1.0), 3,
-           KMEANS_PP_CENTERS, centers);
-
-    // reshape both to a single row of Vec3f pixels:
-    centers = centers.reshape(5,centers.rows);
-    data = data.reshape(5,data.rows);
-    
-
-    //assign colors to centers
-
-
-    // replace pixel values with their center value:
-    Vec<float,5> *p = data.ptr<Vec<float,5>>();
-    for (size_t i=0; i<data.rows; i++) {
-        int center_id = labels.at<int>(i);
-        p[i] = centers.at<Vec<float,5>>(center_id);
+    Mat labels,comp,centroids;
+    int nLabels = connectedComponentsWithStats(dst,labels,comp,centroids);
+    std::vector<Vec3b> colors(nLabels);
+    colors[0] = Vec3b(0, 0, 0);//background
+    for(int label = 1; label < nLabels; ++label){
+        colors[label] = Vec3b( (rand()&255), (rand()&255), (rand()&255) );
     }
 
-    // back to 2d, and uchar:
-    img = data.reshape(3, img.rows);
-    img.convertTo(img, CV_8U);
-
-    imshow("segmented", img);
+    src.convertTo(src,CV_8UC3);
+    for(int r = 0; r < src.rows; ++r){
+        for(int c = 0; c < src.cols; ++c){
+            int label = labels.at<int>(r, c);
+            Vec3b &pixel = src.at<Vec3b>(r, c);
+            pixel = colors[label];
+        }
+    }
+    imshow( window_name, src );
     waitKey(0);
-
     return 0;
 }
