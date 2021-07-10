95883a8a0858eaaa140797ab66eae4c3ffc52462
MattiaFanan <mattia.fanan@gmail.com>
2021-07-10 03:01:02 +0200

    10k ROIs returned but all boats are contained there, need to remove repetitions and intelligent removal of non boat patch

--- src/main.cpp
+++ src/main.cpp
@@ -3,53 +3,51 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+#include<opencv2/ximgproc/edgeboxes.hpp>
+#include<opencv2/ximgproc.hpp>
+#include <X11/Xlib.h>
 
 using namespace cv;
 using namespace std;
-using namespace saliency;
+using namespace ximgproc;
+
+
 
 int main() {
-    Mat img = imread("/home/mattia/CLionProjects/CV/BoatDetector/FINAL_DATASET/TRAINING_DATASET/IMAGES/image0147.png");
-    //cvtColor(img, img, COLOR_RGB2GRAY);
-    /*
-    Ptr<saliency::StaticSaliencyFineGrained> pointer = saliency::StaticSaliencyFineGrained::create();
-    Mat out;
-    Mat threshold_out;
-    pointer->computeSaliency(img, out);
-    out.convertTo(out, CV_8UC1, 255);
-    threshold(out, threshold_out, 0, 255, THRESH_BINARY | THRESH_OTSU);
-    imshow("TMP", img);
-    waitKey(0);
-    imshow("TMP", threshold_out);
-    waitKey(0);
-     */
-    String training_path = "/home/mattia/Dev/OpenCV_installation/opencv_contrib-master/modules/saliency/samples/ObjectnessTrainedModel";
-    Ptr<Saliency> saliencyAlgorithm = ObjectnessBING::create();
-    saliencyAlgorithm.dynamicCast<ObjectnessBING>()->setTrainingPath( training_path );
-    saliencyAlgorithm.dynamicCast<ObjectnessBING>()->setBBResDir( "Results" );
-    vector<Vec4i> saliencyMap;
-    vector<Vec4i> ROIs;
-    Mat out;
-    img.copyTo(out);
-    Scalar color = Scalar(0,0, 255);
-    saliencyAlgorithm.dynamicCast<ObjectnessBING>()->computeSaliency(out, ROIs);
+    Display* disp = XOpenDisplay(NULL);
+    Screen*  scrn = DefaultScreenOfDisplay(disp);
 
-    vector<float> score = saliencyAlgorithm.dynamicCast<ObjectnessBING>()->getobjectnessValues();
+    Mat img = imread("/home/mattia/CLionProjects/CV/BoatDetector/FINAL_DATASET/TRAINING_DATASET/IMAGES/image3161.png");
+    //Mat img = imread("/home/mattia/CLionProjects/CV/BoatDetector/FINAL_DATASET/TEST_DATASET/kaggle/10.jpg");
+    //Mat img = imread("/home/mattia/CLionProjects/CV/BoatDetector/FINAL_DATASET/TEST_DATASET/venice/01.png");
+    resize(img,img,Size(scrn->width - 100,scrn->height - 100));
+    imshow("orig",img);
 
 
-    int max_det = 200;
+    Mat tmp;
+    img.convertTo(tmp,CV_32FC3,1/255.0);
+    Mat edges,orient,suppEdges;
+    Ptr<StructuredEdgeDetection> edgeDetector = createStructuredEdgeDetection("/home/mattia/Downloads/model.yml.gz");
+    edgeDetector->detectEdges(tmp,edges);
+    edgeDetector->computeOrientation(edges,orient);
+    //edge suppression
+    edgeDetector->edgesNms(edges,orient,suppEdges);
+
+    vector<Rect> ROIs;
+    Ptr<EdgeBoxes> boxes = createEdgeBoxes();
+    //boxes->setMaxBoxes(30);
+    boxes->getBoundingBoxes(suppEdges,orient,ROIs);
+
+    Scalar color = Scalar(0,255,0);
+    int max_det = ROIs.size();
     for(ulong i = 0; i < max_det; i++){
-        Vec4i ROI = ROIs[i];
-        img.copyTo(out);
-        putText(out, to_string(score[i]), Point(ROI[0]+20, ROI[1]+20), FONT_HERSHEY_SCRIPT_SIMPLEX, 0.5, color, 2, LINE_AA);
-        rectangle(out, Point(ROI[0], ROI[1]), Point(ROI[2], ROI[3]), color);
-        imshow("TMP", out);
+        img.copyTo(tmp);
+        //putText(out, to_string(score[i]), Point(ROI[0]+20, ROI[1]+20), FONT_HERSHEY_SCRIPT_SIMPLEX, 0.5, color, 2, LINE_AA);
+        rectangle(tmp, ROIs[i], color,2);
+        imshow("TMP", tmp);
         waitKey(0);
     }
 
-    imshow("TMP", img);
-    waitKey(0);
-    imshow("TMP", out);
     waitKey(0);
     return 0;
 }
