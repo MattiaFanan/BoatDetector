d3124c728a45fa39f621a61aebde397550c94803
MattiaFanan <mattia.fanan@gmail.com>
2021-07-13 20:33:55 +0200

    integrated the CNN, problems with venice city elements and a lot of intersecting regions returned

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,13 @@
 #include<opencv2/ximgproc/edgeboxes.hpp>
 #include<opencv2/ximgproc.hpp>
 #include <X11/Xlib.h>
+#include <opencv2/dnn/dnn.hpp>
+#include <iostream>
 
 using namespace cv;
 using namespace std;
 using namespace ximgproc;
+using namespace dnn;
 
 vector<Rect> removeDuplicates(const vector<Rect> &input, double dimensionSlackPerc);
 bool areSimilarRects(const Rect &r1, const Rect &r2, double dimensionSlackPerc);
@@ -15,10 +18,14 @@ bool areSimilarRects(const Rect &r1, const Rect &r2, double dimensionSlackPerc);
 int main() {
     Display* disp = XOpenDisplay(NULL);
     Screen*  scrn = DefaultScreenOfDisplay(disp);
+    Net net = readNetFromTensorflow("/home/mattia/CLionProjects/CV/BoatDetector/models/naive_dataset_model.pb"); //Load the model
+
+    net.setPreferableBackend(DNN_BACKEND_DEFAULT);
+    net.setPreferableTarget(DNN_TARGET_CPU);
 
     //Mat img = imread("/home/mattia/CLionProjects/CV/BoatDetector/FINAL_DATASET/TRAINING_DATASET/IMAGES/image3161.png");
     //Mat img = imread("/home/mattia/CLionProjects/CV/BoatDetector/FINAL_DATASET/TEST_DATASET/kaggle/10.jpg");
-    Mat img = imread("/home/mattia/CLionProjects/CV/BoatDetector/FINAL_DATASET/TEST_DATASET/venice/05.png");
+    Mat img = imread("/home/mattia/CLionProjects/CV/BoatDetector/FINAL_DATASET/TEST_DATASET/venice/06.png");
     resize(img,img,Size(scrn->width - 100,scrn->height - 100));
     imshow("orig",img);
 
@@ -40,15 +47,24 @@ int main() {
     double dimensionSlackPerc = 0.1;
     ROIs = removeDuplicates(ROIs, dimensionSlackPerc);
 
-
+    img.copyTo(tmp);
     Scalar color = Scalar(0,255,0);
     for(Rect ROI : ROIs){
-        img.copyTo(tmp);
-        rectangle(tmp, ROI, color,2);
-        imshow("TMP", tmp);
-        waitKey(0);
+        //img.copyTo(tmp);
+        Mat input = img(ROI);
+        vector<Mat> out;
+        input = blobFromImage(input,1.0/255,Size(100,100),Scalar(),false,false,CV_32F);
+        net.setInput(input);
+        net.forward(out);
+        float perc = out.at(0).at<float>(0,0);
+        if(perc>=0.95)
+            rectangle(tmp, ROI, color,2);
+        //putText(tmp, to_string(perc),Point(50,50),FONT_HERSHEY_SCRIPT_SIMPLEX,0.5,color);
+        //rectangle(tmp, ROI, color,2);
+        //imshow("TMP", tmp);
+        //waitKey(0);
     }
-
+    imshow("tmp",tmp);
     waitKey(0);
     return 0;
 }
