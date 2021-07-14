3f27e791e42a03ff3e0f7c520953436fab5d1272
MattiaFanan <mattia.fanan@gmail.com>
2021-07-14 21:24:35 +0200

    non maxima suppression

--- src/main.cpp
+++ src/main.cpp
@@ -18,16 +18,15 @@ bool areSimilarRects(const Rect &r1, const Rect &r2, double dimensionSlackPerc);
 int main() {
     Display* disp = XOpenDisplay(NULL);
     Screen*  scrn = DefaultScreenOfDisplay(disp);
-    Net net = readNetFromTensorflow("/home/mattia/CLionProjects/CV/BoatDetector/models/naive_dataset_model.pb"); //Load the model
+    Net net = readNetFromTensorflow("/home/mattia/CLionProjects/CV/BoatDetector/models/sp_over_se_model.pb"); //Load the model
 
     net.setPreferableBackend(DNN_BACKEND_DEFAULT);
     net.setPreferableTarget(DNN_TARGET_CPU);
 
     //Mat img = imread("/home/mattia/CLionProjects/CV/BoatDetector/FINAL_DATASET/TRAINING_DATASET/IMAGES/image3161.png");
     //Mat img = imread("/home/mattia/CLionProjects/CV/BoatDetector/FINAL_DATASET/TEST_DATASET/kaggle/10.jpg");
-    Mat img = imread("/home/mattia/CLionProjects/CV/BoatDetector/FINAL_DATASET/TEST_DATASET/venice/06.png");
+    Mat img = imread("/home/mattia/CLionProjects/CV/BoatDetector/FINAL_DATASET/TEST_DATASET/venice/07.png");
     resize(img,img,Size(scrn->width - 100,scrn->height - 100));
-    imshow("orig",img);
 
     Mat tmp;
     img.convertTo(tmp,CV_32FC3,1/255.0);
@@ -47,24 +46,30 @@ int main() {
     double dimensionSlackPerc = 0.1;
     ROIs = removeDuplicates(ROIs, dimensionSlackPerc);
 
-    img.copyTo(tmp);
-    Scalar color = Scalar(0,255,0);
+    //classification
+    vector<float> scores;
     for(Rect ROI : ROIs){
-        //img.copyTo(tmp);
         Mat input = img(ROI);
         vector<Mat> out;
         input = blobFromImage(input,1.0/255,Size(100,100),Scalar(),false,false,CV_32F);
         net.setInput(input);
         net.forward(out);
-        float perc = out.at(0).at<float>(0,0);
-        if(perc>=0.95)
-            rectangle(tmp, ROI, color,2);
-        //putText(tmp, to_string(perc),Point(50,50),FONT_HERSHEY_SCRIPT_SIMPLEX,0.5,color);
-        //rectangle(tmp, ROI, color,2);
-        //imshow("TMP", tmp);
-        //waitKey(0);
+        scores.push_back(out.at(0).at<float>(0,0));
     }
-    imshow("tmp",tmp);
+
+    // non maxima suppression
+    float score_thresh = 0.8;
+    float nms_thresh = 0.1;
+    vector<int> keptIndices;
+    NMSBoxes(ROIs,scores,score_thresh,nms_thresh,keptIndices);
+
+    //draw regions
+    Scalar color = Scalar(0,255,0);
+    for(int i : keptIndices){
+        putText(img, to_string(scores[i]),ROIs[i].tl() + Point(20,20),FONT_HERSHEY_SCRIPT_SIMPLEX,0.5,color);
+        rectangle(img, ROIs[i], color,2);
+    }
+    imshow("detection",img);
     waitKey(0);
     return 0;
 }
